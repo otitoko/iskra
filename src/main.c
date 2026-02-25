3fab44b2a63b20773f43b0537ed6d2b51dd603e6
otitoko <tbkopera@proton.me>
2026-02-25 20:44:01 +0000

    removed pwd function and implemented proper dynamically allocated tokenization buffers

--- src/main.c
+++ src/main.c
@@ -5,7 +5,13 @@
 
 #include "builtins/echo.h"
 
+char* builtin_str[] = {
+	"echo"
+};
 
+int (*builtin_functions[]) (char*) = {
+	&echo
+};
 
 int print_prompt(){
 
@@ -17,16 +23,35 @@ int print_prompt(){
 	return 0;
 }
 
-int parse_input(char* s){
+//take input as s and return pointer to tokens
+char** parse_input(char* s){
+
+	int index = 0;
+	int buf_size = 0;
+
+	if(s == NULL){
+		//end
+	//	return s;
+	}
 	char* token = strtok(s," ");
+	
+	char** tokens = malloc(sizeof(char*));
+	buf_size+=sizeof(char*);
+
+	tokens[0] = token;
+	index++;
 
 	while(token != NULL){
-
-	printf("%s", token);
+	
 	token = strtok(NULL," ");
+
+	tokens = (char**) realloc(tokens, buf_size+sizeof(char*));
+	tokens[index] = token;
+	buf_size+=sizeof(char*);
+	index++;
 	}
 
-	return 0;
+	return tokens;
 }
 
 int main(){
@@ -35,9 +60,13 @@ int main(){
 		print_prompt();
 		char s[64];
 		fgets(s,sizeof(s)-1,stdin);
-		parse_input(s);
+		char **tokens = parse_input(s);
+		int i = 0;
+		do { 
+			printf("%s\n", tokens[i]); 
+			i++;
+		} while (tokens[i] != NULL);
 	}
 
 	return 0;
 }
-
