41861b75f42dd28e08021dfad285290c752675d8
otitoko <tbkopera@proton.me>
2026-04-08 15:06:36 +0100

    implemented < operator

--- include/parsing/parsing.h
+++ include/parsing/parsing.h
@@ -6,11 +6,12 @@ extern const int MANIP_STR_NUM;
 char** tokenize(char* s);
 int count_tokens(char** tokens);
 int redirect_output(char** strarr, char* redirect);
+int redirect_input(char** strarr, char* redirect);
 int check_tokens(char** tokens);
 int eval_redirect(int symbol,char* string, char** tokens, char* redirect);
 char* return_string(char** tokens, int lower, int upper);
 int find_upper(char** tokens);
-int reset_fd(int saved_state);
+int reset_fd(int saved_state,int stream);
 char** trim_tokens(int argc, char** argv);
 
 extern char* manip_str[];
